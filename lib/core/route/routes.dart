fbfd1757b0c6cf6abde9df1b3e16955dc3a1ae5d
Jahid01234 <mdjahidhasan790523@gmail.com>
2025-05-16 23:06:21 +0600

    - welcome screen - onboarding screen modify

--- lib/core/route/routes.dart
+++ lib/core/route/routes.dart
@@ -1,4 +1,8 @@
+import 'package:food_delivery_application/feature/auth/login/view/login_screen.dart';
+import 'package:food_delivery_application/feature/auth/register/view/signup_screen.dart';
+import 'package:food_delivery_application/feature/onboarding/views/onboarding_screen.dart';
 import 'package:food_delivery_application/feature/splash/view/splash_screen.dart';
+import 'package:food_delivery_application/feature/welcome/view/welcome_screen.dart';
 import 'package:get/get.dart';
 
 class AppRoutes {
@@ -6,9 +10,10 @@ class AppRoutes {
   // Get routes name here.......
   static const String splash = '/splash';
   static const String onboarding = '/onboarding';
+  static const String welcome = '/welcome';
   static const String login = '/login';
   static const String register = '/register';
-  static const String forgetPassword = '/forgetPassword';
+
 
   // Get routes here.......
   static List<GetPage> routes = [
@@ -18,13 +23,11 @@ class AppRoutes {
       transition: Transition.leftToRight,
     ),
 
+    GetPage(name: onboarding, page: () => OnboardingScreen()),
+    GetPage(name: welcome, page: () => WelcomeScreen()),
+    GetPage(name: login, page: () => LoginScreen()),
+    GetPage(name: register, page: () => SignUpScreen()),
 
-    // auth start..
-    // GetPage(name: onboarding, page: () => OnboardingScreen()),
-    // GetPage(name: login, page: () => LoginScreen()),
-    // GetPage(name: register, page: () => RegisterScreen()),
-    // GetPage(name: forgetPassword, page: () => ForgetPasswordScreen()),
-    //
 
 
   ];
